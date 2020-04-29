7de468fc8c293bcc43eec51f38dac057ca1cd8e0
Josh Walawender <jmwalawender@gmail.com>
2020-04-28 15:13:59 -1000

    remove files to clear out repo

--- moveTel.py
+++
@@ -1,3 +0,0 @@
-import TelescopeControl
-
-##This script will be a command line way to use the TelescopeControl class
--- moveTelGUI.py
+++
@@ -1,3 +0,0 @@
-import TelescopeControl
-
-##This file will have the Gui control of the TelescopeControl class
